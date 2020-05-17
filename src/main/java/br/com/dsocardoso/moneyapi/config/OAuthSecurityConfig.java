dc4f0a8c869630903b730faa60df3ca85f616f0c
Diego Semensato O Cardoso <dsocardoso@gmail.com>
2020-05-17 00:43:03 -0300

    autenticando usuario por consulta ao banco de dados

--- src/main/java/br/com/dsocardoso/moneyapi/config/OAuthSecurityConfig.java
+++ src/main/java/br/com/dsocardoso/moneyapi/config/OAuthSecurityConfig.java
@@ -1,9 +1,7 @@
 package br.com.dsocardoso.moneyapi.config;
 
-import org.graalvm.compiler.lir.StandardOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
--- src/main/java/br/com/dsocardoso/moneyapi/config/ResourceServerConfig.java
+++ src/main/java/br/com/dsocardoso/moneyapi/config/ResourceServerConfig.java
@@ -1,12 +1,16 @@
 package br.com.dsocardoso.moneyapi.config;
 
 
+import br.com.dsocardoso.moneyapi.security.AppUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -17,9 +21,15 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*
         auth.inMemoryAuthentication()
                 .withUser("admin").password("admin").roles("ROLE");
+         */
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -34,5 +44,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.stateless(true);
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
